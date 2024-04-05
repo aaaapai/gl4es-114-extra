126f670e9705c9a6094770685bb18638ff0249ba
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-04-05 21:56:27 +0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -89,11 +89,12 @@ LOCAL_SRC_FILES := \
 	src/glx/streaming.c \
 	src/gl/vgpu/shaderconv.c\
 
-LOCAL_CFLAGS += -g -std=gnu99 -funwind-tables -O3 -fvisibility=hidden -include include/android_debug.h
+LOCAL_CFLAGS += -g -std=gnu99 -funwind-tables -Ofast -fvisibility=hidden -include include/android_debug.h
 LOCAL_CFLAGS += -DNOX11
 LOCAL_CFLAGS += -DNO_GBM
 #LOCAL_CFLAGS += -DNO_INIT_CONSTRUCTOR
 #LOCAL_CFLAGS += -DDEFAULT_ES=2
+LOCAL_CFLAGS += -DANDROID -pipe -integrated-as -fno-plt -Ofast -flto -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-run-dce
 
 LOCAL_LDLIBS := -ldl -llog
 #building as a static lib
